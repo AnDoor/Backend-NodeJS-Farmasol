bd8f097d4f12c529d55813459befd222dd7f0ff2
Andrew <axpv33@gmail.com>
2025-03-07 23:51:40 -0400

    actualizacion de backend BD

--- src/bd.sql
+++ src/bd.sql
@@ -40,3 +40,28 @@ INSERT INTO empleado_cargo (empleado, cargo, diaQueTrabajo) VALUES
 (52345678, 3, '2023-10-03'),  -- Carlos López (Auxiliar de Farmacia) trabajó el 3 de octubre de 2023
 (63456789, 4, '2023-10-04'),  -- Ana Martínez (Pasante) trabajó el 4 de octubre de 2023
 (74567890, 5, '2023-10-05');  -- Luis Rodríguez (Analista de Compra) trabajó el 5 de octubre de 2023
+
+CREATE TABLE medicamentoStocks (code SERIAL PRIMARY KEY, sucursal INT, FOREIGN KEY (sucursal) REFERENCES sucursal(idsucur
+sal), medicamento INT, FOREIGN KEY (medicamento) REFERENCES medicamento(idmedicamento), stock INT);
+
+CREATE TABLE medicamento_mono (id SERIAL PRIMARY KEY, medicamento VARCHAR(50), FOREIGN KEY (medicamento) REFERENCES medic
+amento(code), monodroga INT, FOREIGN KEY (monodroga) REFERENCES monodroga(code));
+
+CREATE TABLE accion_t (id SERIAL PRIMARY KEY, nombre VARHCAR(50), descripcion VARCHAR(100));
+
+CREATE TABLE medicamento_accion(id SERIAL PRIMARY KEY, medicamento VARCHAR(50), FOREIGN KEY(medicamento) REFERENCES medic
+amento(code), accionTerapeutica INT, FOREIGN KEY (accionTerapeutica) REFERENCES accion_t(id));
+
+CREATE TABLE empleado_reporte_sucursal (code SERIAL PRIMARY KEY, sucursal INT, FOREIGN KEY (sucursal)REFERENCES sucursal(
+idsucursal),empleado INT, FOREIGN  KEY (empleado) REFERENCES empleado(idempleado), fecha_i DATE, fecha_f DATE);
+
+CREATE TABLE laboratorio (RIF varchar(2
+0) PRIMARY KEY, nombre VARCHAR(50) NOT NULL, direc
+cion VARCHAR(50) NOT NULL, telefono VARCHAR(16) NO
+T NULL, email VARCHAR(50));
+
+CREATE TABLE laboratorio_medicamento(id
+ SERIAL PRIMARY KEY, medicamento VARCHAR(50), FORE
+IGN KEY (medicamento) REFERENCES medicamento(code)
+, laboratorio VARCHAR(20), FOREIGN KEY (laboratori
+o) REFERENCES laboratorio(RIF), stock INT)
